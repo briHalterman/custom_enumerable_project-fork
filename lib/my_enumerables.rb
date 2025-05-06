33d1293a666f62f233377a9a958749e1daabecc4
Brittany Halterman <halterma@gmail.com>
2025-05-06 07:40:32 -0400

    Define my_map

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,12 @@
 module Enumerable
   # Your code goes here
+  def my_map
+    new_array = []
+    for item in self
+      new_array << yield(item)
+    end
+    new_array
+  end
 end
 
 # You will first have to define my_each
@@ -14,3 +21,5 @@ class Array
     end
   end
 end
+
+[1, 2, 3].map(&:even?)
